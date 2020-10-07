887df0b14f6c10f1dac6eba3228f456ba28923da
Arkadiusz <arkadiusz.latka@sembot.io>
2020-10-07 20:06:13 +0200

    add basic models

---
+++ src/app/dashboard/customers/customer.model.ts
@@ -0,0 +1,7 @@
+export interface CustomerModel {
+  id: number;
+  name: string;
+  surname: string;
+  phone: number;
+  info: string;
+}
---
+++ src/app/dashboard/message/message.model.ts
@@ -0,0 +1,8 @@
+import { CustomerModel } from '../customers/customer.model';
+
+export interface MessageModel {
+  id: number;
+  name: string;
+  text: string;
+  customer: CustomerModel;
+}
