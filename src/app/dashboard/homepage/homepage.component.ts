052b27eb35b2244dc63cb406fc5e1bc6175a2748
ballkar <arkadiusz.latka@o2.pl>
2021-03-10 14:07:57 +0100

    move logic to own service

--- src/app/dashboard/homepage/homepage.component.ts
+++ src/app/dashboard/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrientationConfiguration, Orientation, TourStep, GuidedTour, GuidedTourService } from 'ngx-guided-tour';
 import { AuthService } from 'src/app/auth/auth.service';
+import { TutorialService } from '../services/tutorial.service';
 import { UserService } from '../user/user.service';
 
 @Component({
@@ -9,34 +10,9 @@ import { UserService } from '../user/user.service';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
-  config: OrientationConfiguration = {
-    orientationDirection: Orientation.Center
-  };
-  stepOne: TourStep = {
-    content: 'test',
-    skipStep: false,
-  }
-  stepTwo: TourStep = {
-    content: 'test2',
-    skipStep: false,
-  }
-  stepThree: TourStep = {
-    content: 'test3',
-    skipStep: false,
-  }
-  tour: GuidedTour = {
-    steps: [
-      this.stepOne,
-      this.stepTwo,
-      this.stepThree,
-    ],
-    tourId: 'base',
-    completeCallback: () => console.log('complete'),
-  }
-
   constructor(
-    private guidedTourService: GuidedTourService,
     private authServie: UserService,
+    private tutorialService: TutorialService,
   ) {
     if(!this.authServie.loggedUser.tutorialComplete) {
       setTimeout(() => this.startTour(), 1000);
@@ -47,7 +23,7 @@ export class HomepageComponent implements OnInit {
   }
 
   startTour(): void {
-    this.guidedTourService.startTour(this.tour);
+    this.tutorialService.startBaseTutorial();
   }
 
 }
--- src/app/dashboard/services/tutorial.service.ts
+++ src/app/dashboard/services/tutorial.service.ts
@@ -1,18 +1,43 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { OrientationConfiguration, Orientation, TourStep, GuidedTour, GuidedTourService } from 'ngx-guided-tour';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TutorialService {
-  open = false;
-  opened$: Subject<boolean> = new Subject<boolean>();
+  config: OrientationConfiguration = {
+    orientationDirection: Orientation.Center
+  };
+  stepOne: TourStep = {
+    content: 'test',
+    skipStep: false,
+  }
+  stepTwo: TourStep = {
+    content: 'test2',
+    skipStep: false,
+  }
+  stepThree: TourStep = {
+    content: 'test3',
+    skipStep: false,
+  }
+
+  baseTour: GuidedTour = {
+    steps: [
+      this.stepOne,
+      this.stepTwo,
+      this.stepThree,
+    ],
+    tourId: 'base',
+    completeCallback: () => console.log('complete'),
+  }
 
   constructor(
-  ) { }
+    private guidedTourService: GuidedTourService,
+  ) {
 
-  toggle() {
-    this.open = !this.open;
-    this.opened$.next(this.open);
+  }
+
+  startBaseTutorial() {
+    this.guidedTourService.startTour(this.baseTour);
   }
 }
